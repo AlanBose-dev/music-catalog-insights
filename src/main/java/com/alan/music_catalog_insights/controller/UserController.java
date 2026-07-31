d20fb4f581e9aecc9f6edad0024a5009047758d1
AlanBose-dev <alanbose701@gmail.com>
2026-07-31 08:18:08 +0530

    Completed user registration with BCrypt password encryption

---
+++ src/main/java/com/alan/music_catalog_insights/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.alan.music_catalog_insights.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alan.music_catalog_insights.entity.User;
+import com.alan.music_catalog_insights.service.UserService;
+
+@RequestMapping("/api")
+@RestController
+public class UserController {
+	private final UserService userService;
+
+	@GetMapping("/test")
+	public String test() {
+	    return "Backend Working";
+	}
+	
+	@PostMapping("/register")
+	public User registerUser(@RequestBody User user) {
+
+	    return userService.registerUser(user);
+
+	}
+	
+	public UserController(UserService userService) {
+	    this.userService = userService;
+	}
+
+	public UserService getUserService() {
+		return userService;
+	}
+}
--- src/main/java/com/alan/music_catalog_insights/repository/UserRepository.java
+++ src/main/java/com/alan/music_catalog_insights/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.alan.music_catalog_insights.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alan.music_catalog_insights.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
